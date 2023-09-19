9b3302a938685347c04ecc08bc4f3ed6f3d19222
sujay r <sujaysy0006@gmail.com>
2023-09-20 00:33:34 +0530

    added features

--- src/app.js
+++ src/app.js
@@ -10,10 +10,19 @@ app.use( favicon( path.join( __dirname, 'favicon.ico' ) ) );
 app.use( '/assets', express.static( path.join( __dirname, 'assets' ) ) );
 
 app.get( '/', ( req, res ) => {
+    res.sendFile( __dirname + '/home.html' );
+});
+app.get( '/login', ( req, res ) => {
+    res.sendFile( __dirname + '/login.html' );
+});
+app.get( '/signup', ( req, res ) => {
+    res.sendFile( __dirname + '/signup.html' );
+});
+app.get( '/video', ( req, res ) => {
     res.sendFile( __dirname + '/index.html' );
-} );
+});
 
 
 io.of( '/stream' ).on( 'connection', stream );
 
-server.listen(3000);
+server.listen(3000 );
